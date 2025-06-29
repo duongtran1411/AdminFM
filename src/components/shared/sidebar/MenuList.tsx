4bbd491b4f15696e00b02651f3191bfb8b0cfb4c
duongtran1411 <duongtdhe172042@fpt.edu.vn>
2025-06-29 23:24:56 +0700

    duong fix sidebar

--- src/components/shared/sidebar/MenuList.tsx
+++ src/components/shared/sidebar/MenuList.tsx
@@ -1,12 +1,16 @@
 import { Menu } from "antd";
 import { ItemType, MenuItemType } from "antd/es/menu/interface";
 import {
-  AiFillBank
+  AiFillBank,
+  AiOutlineFolder
 } from "react-icons/ai";
 import { PiStudent } from "react-icons/pi";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useTheme } from "../../../contexts/ThemeContext";
 import { useState } from "react";
+import { HomeOutlined } from "@ant-design/icons";
+import { FaRegUser } from "react-icons/fa";
+import { MdGrade, MdOutlineStarRate } from "react-icons/md";
 
 const DashBoardMenuList = () => {
   const { theme, colorBgContainer } = useTheme();
@@ -32,76 +36,76 @@ const DashBoardMenuList = () => {
 
   const selectedKey = location.pathname.slice(1) || "dashboards";
   const menuItems: ItemType<MenuItemType>[] = [
-    // { key: "dashboards", icon: <HomeOutlined />, label: "Dashboards" },
-    // { key: "users", icon: <FaRegUser />, label: "Users" },
-    // {
-    //   key: "tuyensinh",
-    //   label: "Tuyển Sinh",
-    //   icon: <HiAcademicCap />,
-    //   children: [
-    //     {
-    //       key: "admission",
-    //       icon: <LuBookCopy />,
-    //       label: "CT Tuyển Sinh",
-    //     },
-    //     {
-    //       key: "application",
-    //       icon: <VscGitStashApply />,
-    //       label: "DS Ứng tuyển",
-    //     },
-    //     {
-    //       key: "freshmens",
-    //       icon: <FaChalkboardTeacher />,
-    //       label: "DS Nhập học",
-    //     },
-    //     {
-    //       key: "promotions",
-    //       icon: <AiOutlineNotification />,
-    //       label: "CT Ưu Đãi",
-    //     },
-    //     {
-    //       key: "applicationdocument",
-    //       icon: <AiOutlineFolder />,
-    //       label: "TP hồ sơ",
-    //     },
-    //   ],
-    // },
+    { key: "dashboards", icon: <HomeOutlined />, label: "Dashboards" },
+    { key: "users", icon: <FaRegUser />, label: "Users" },
+    {
+      key: "tuyensinh",
+      label: "Tuyển Sinh",
+      icon: <AiOutlineFolder />,
+      children: [
+        {
+          key: "admission",
+          icon: <AiOutlineFolder />,
+          label: "CT Tuyển Sinh",
+        },
+        {
+          key: "application",
+          icon: <AiOutlineFolder />,
+          label: "DS Ứng tuyển",
+        },
+        {
+          key: "freshmens",
+          icon: <AiOutlineFolder />,
+          label: "DS Nhập học",
+        },
+        {
+          key: "promotions",
+          icon: <AiOutlineFolder />,
+          label: "CT Ưu Đãi",
+        },
+        {
+          key: "applicationdocument",
+          icon: <AiOutlineFolder />,
+          label: "TP hồ sơ",
+        },
+      ],
+    },
     {
       key: "center",
       label: "Center",
       icon: <AiFillBank />,
       children: [
         { key: "students", icon: <PiStudent />, label: "Sinh viên" },
-        // { key: "classes", icon: <SiGoogleclassroom />, label: "Lớp học" },
-        // { key: "teachers", icon: <FaChalkboardTeacher />, label: "Giảng viên" },
-        // { key: "building", icon: <FaRegBuilding />, label: "Toà nhà" },
-        // { key: "cohort", icon: <GoPeople />, label: "Niên khóa" },
-        // { key: "examschedule", icon: <AiOutlineFolder />, label: "Lịch thi" },
+        { key: "classes", icon: <AiOutlineFolder />, label: "Lớp học" },
+        { key: "teachers", icon: <AiOutlineFolder />, label: "Giảng viên" },
+        { key: "building", icon: <AiOutlineFolder />, label: "Toà nhà" },
+        { key: "cohort", icon: <AiOutlineFolder />, label: "Niên khóa" },
+        { key: "examschedule", icon: <AiOutlineFolder />, label: "Lịch thi" },
       ],
     },
-    // {
-    //   key: "academic",
-    //   label: "Academic",
-    //   icon: <HiAcademicCap />,
-    //   children: [
-    //     { key: "coursefamily", icon: <LuBookCopy />, label: "Courses Family" },
-    //     { key: "courses", icon: <LuBookMarked />, label: "Courses" },
-    //     { key: "module", icon: <AiOutlineFolder />, label: "Module" },
-    //     // Thêm menulist vào sau nếu cần
-    //   ],
-    // },
-    // { key: "markreport", icon: <MdGrade />, label: "Báo cáo điểm" },
-    // {
-    //   key: "evaluation",
-    //   icon: <MdOutlineStarRate />,
-    //   label: "Đánh giá cá nhân",
-    // },
-    // { key: "re-register-module", icon: <MdGrade />, label: "Đăng ký học lại" },
-    // {
-    //   key: "student-resit",
-    //   icon: <MdGrade />,
-    //   label: "Danh sách đăng ký học lại",
-    // },
+    {
+      key: "academic",
+      label: "Academic",
+      icon: <AiOutlineFolder />,
+      children: [
+        { key: "coursefamily", icon: <AiOutlineFolder />, label: "Courses Family" },
+        { key: "courses", icon: <AiOutlineFolder />, label: "Courses" },
+        { key: "module", icon: <AiOutlineFolder />, label: "Module" },
+        // Thêm menulist vào sau nếu cần
+      ],
+    },
+    { key: "markreport", icon: <MdGrade />, label: "Báo cáo điểm" },
+    {
+      key: "evaluation",
+      icon: <MdOutlineStarRate />,
+      label: "Đánh giá cá nhân",
+    },
+    { key: "re-register-module", icon: <MdGrade />, label: "Đăng ký học lại" },
+    {
+      key: "student-resit",
+      icon: <MdGrade />,
+      label: "Danh sách đăng ký học lại",
+    },
   ];
 
   const getAllParentKeys = () => {
