61c07d05cf84a894571a315142b0850e76d4a50c
duongtran1411 <duongtdhe172042@fpt.edu.vn>
2025-07-03 04:14:13 +0700

    duong

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -43,7 +43,7 @@ const UserPage = () => {
     {
       title: "STT",
       key: "STT",
-      render: (index: number) => index + 1,
+      render: (_text: any, _record: Users, index: number) => index + 1,
     },
     {
       title: "Tên người dùng",
